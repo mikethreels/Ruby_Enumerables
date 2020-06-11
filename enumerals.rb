d78b18193604dfd5b9e36b3a82bf8833089cca7d
Michael Threels <michaelthreels@gmail.com>
2020-06-11 21:15:09 +0200

    added my_none errors still to be fixed

--- enumerals.rb
+++ enumerals.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# rubocop:disable Style/CaseEquality
+
+
 # This module copies the Enumerable Module embedded with Ruby
 module Tools
   def my_each
@@ -47,13 +50,27 @@ module Tools
     org_arr = true
   end
 
-  def my_any?
-    return true unless block_given?
-
+  def my_none?
     org_arr = self
-    org_arr.length.times do |i|
-      return true if yield(org_arr[i])
+    return true if org_arr.empty?
+
+    if block_given?
+      org_arr.length.times do |i|
+        return true unless yield(org_arr[i])
+
+        break
+      end
+    else
+      org_arr.length.times do |i|
+        if org_arr[i] == false || org_arr[i].nil?
+          true
+        elsif org_arr[i]
+          return false
+        end
+      end
     end
     org_arr = false
   end
 end
+
+# rubocop:enable Style/CaseEquality
--- test.rb
+++ test.rb
@@ -1,9 +1,13 @@
 require_relative 'enumerals.rb'
 include Tools
-my_array = [5, 4, 6, 7]
+my_array = [nil, false, true]
 # p my_array.my_each { |value| p value }
 # p my_array.my_each_with_index { |value, i| p value, i }
 # p my_array.my_select { |value| p value.odd? }
 # p my_array.my_all? { |value| p value > 3 }
-p my_array.my_any? { |value| p value > 6}
+# p my_array.my_any? { |value| p value > 6}
+
+p my_array.my_none? { |value| p value > 3 }
+puts "hello world"
+p my_array.none? { |value| p value > 3 }
 
