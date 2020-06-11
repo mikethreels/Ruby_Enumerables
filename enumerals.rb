44386c0b557eac4d602b1ff0118fc65181823f17
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2020-06-11 12:41:57 -0300

    Added my_select and corrected Rubocop Linters

--- enumerals.rb
+++ enumerals.rb
@@ -1,23 +1,39 @@
+# frozen_string_literal: true
+
+# This module copies the Enumerable Module embedded with Ruby
 module Tools
-
-
   def my_each
     return to_enum(:my_each) unless block_given?
+
+    org_arr = self
     arr = []
-    self.length.times do|i| 
-    yield(self[i])
-    arr << self[i]
+    org_arr.length.times do |i|
+      yield(org_arr[i])
+      arr << org_arr[i]
     end
     arr
   end
 
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
+
+    org_arr = self
     arr = []
-    self.length.times do |i|
-      yield(self[i], i)
-      arr << self[i]
+    org_arr.length.times do |i|
+      yield(org_arr[i], i)
+      arr << org_arr[i]
     end
-  arr
+    arr
+  end
+
+  def my_select
+    return to_enum(:my_select) unless block_given?
+
+    org_arr = self
+    arr = []
+    org_arr.length.times do |i|
+      arr << org_arr[i] if yield(org_arr[i])
+    end
+    arr
   end
 end
--- test.rb
+++ test.rb
@@ -1,6 +1,7 @@
 require_relative 'enumerals.rb'
 include Tools
 my_array = [5, 4, 6, 7]
-# p my_array.my_each { |v| p v }
-# p my_array.my_each_with_index { |v, i| p v, i }
+# p my_array.my_each { |value| p value }
+# p my_array.my_each_with_index { |value, i| p value, i }
 
+p my_array.my_select { |value| p value.odd? }
