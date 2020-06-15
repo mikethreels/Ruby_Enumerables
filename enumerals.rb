6db2eba0e27d92c0a4984b2486809f552733bd1c
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2020-06-15 17:45:49 -0300

    Finished my_map my_inject and corrected Rubocop Errors (maybe)

--- enumerals.rb
+++ enumerals.rb
@@ -1,4 +1,5 @@
 module Enumerable
+  # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
   def my_each
     return to_enum(__method__) unless block_given?
@@ -14,7 +15,6 @@ module Enumerable
 
     size.times do |i|
       yield(to_a[i], i)
-
     end
     self
   end
@@ -23,87 +23,79 @@ module Enumerable
     return to_enum(__method__) unless block_given?
 
     arr = []
-    size.times do |i|
-      arr << to_a[i] if yield(to_a[i])
-    end
+    size.times { |i| arr << to_a[i] if yield(to_a[i]) }
     arr
   end
 
   def my_all?(arg = nil)
     puts "#{__FILE__}:#{__LINE__}: warning: given block not used" if arg && block_given?
-    if arg
-      return check_arg(arg)
-    elsif !block_given?
-      size.times do |i|
-        return false if to_a[i] == false || to_a[i].nil?
-      end
-      return true
-    else
-      my_each do |i|
-        return false unless yield(i)
-      end
-      true
-    end
-  end
+    check_arg(arg) if arg
 
-  def my_any?(arg = nil)
-    puts "#{__FILE__}:#{__LINE__}: warning: given block not used" if arg && block_given?
-    if arg
-      return check_any_arg(arg)
-    elsif !block_given?
-      my_each do |i|
-        return true if i
-      end
-      return false
+    if !block_given?
+      size.times { |i| return false if to_a[i] == false || to_a[i].nil? }
     else
-      my_each do |i|
-        return true if yield(i)
-      end
-      false
-    end
-  end
-
-  def my_none?
-    org_arr = self
-    result = true
-    if block_given?
-      org_arr.my_each { |i| return false if yield(i) }
-    else
-      org_arr.my_each { |i| result = false if i == true || (i.is_a? Integer) }
-      return result
+      my_each { |i| return false unless yield(i) }
     end
     true
   end
 
-  def my_count
-    org_arr = self
-    return org_arr.length unless block_given?
+  def my_any?(arg = nil)
+    puts "#{__FILE__}:#{__LINE__}: warning: given block not used" if arg && block_given?
+    check_any_arg(arg) if arg
+
+    if !block_given?
+      my_each { |i| return true if i }
+    else
+      my_each { |i| return true if yield(i) }
+    end
+    false
+  end
+
+  def my_none?(arg = nil)
+    puts "#{__FILE__}:#{__LINE__}: warning: given block not used" if arg && block_given?
+    return !check_any_arg(arg) if arg
+
+    if !block_given?
+      my_each { |i| return false if i == true }
+    else
+      my_each { |i| return false if yield(i) }
+    end
+
+    true
+  end
+
+  def my_count(arg = nil)
+    puts "#{__FILE__}:#{__LINE__}: warning: given block not used" if arg && block_given?
+    return size if !block_given? && arg.nil?
 
     counter = 0
-    org_arr.my_each { |i| counter += 1 if yield(i) }
+    if arg
+      my_each { |i| counter += 1 if i == arg }
+    else
+      my_each { |i| counter += 1 if yield(i) }
+    end
     counter
   end
 
   def my_map(arg = nil)
-    org_arr = self
+    puts "#{__FILE__}:#{__LINE__}: warning: given block not used" if arg && block_given?
     new_arr = []
 
     return to_enum(:my_map) if !block_given? && arg.nil?
 
-    org_arr.my_each { |i| new_arr << arg.call(i) } unless arg.nil?
+    my_each { |i| new_arr << arg.call(i) } unless arg.nil?
 
-    org_arr.my_each { |i| new_arr << yield(i) } if arg.nil? && block_given?
+    my_each { |i| new_arr << yield(i) } if arg.nil? && block_given?
 
     new_arr
   end
 
-  # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
-
   def my_inject(arg = nil, sym = nil)
     # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+    raise LocalJumpError.new, 'no block given' unless block_given? || arg
 
     org_arr = self
-    array = arg.nil? ? org_arr : org_arr.unshift(arg)
+    array = arg.nil? ? org_arr.to_a : org_arr.to_a.unshift(arg)
     return nil if array.empty?
 
     result = array[0]
@@ -116,36 +108,26 @@ module Enumerable
   end
 
   def multiply_els
-    array = self
-    array.my_inject { |i, a| i * a }
+    my_inject { |i, a| i * a }
   end
 
   def check_arg(arg)
-    if arg.is_a? (Regexp)
-      my_each do |i|
-        return false if (i =~ arg).nil?
-      end
-      return true
-    elsif arg 
-      my_each do |i|
-        return false if !(i.is_a? (arg))
-      end
-      return true
+    if arg.is_a?(Regexp)
+      my_each { |i| return false if (i =~ arg).nil? }
+      true
+    elsif arg
+      my_each { |i| return false unless i.is_a? arg }
+      true
     end
   end
 
   def check_any_arg(arg)
-    if arg.is_a? (Regexp)
-      my_each do |i|
-        return true if !(i =~ arg).nil?
-      end
-      return false
-    elsif arg 
-      my_each do |i|
-        return true if (i.is_a? (arg))
-      end
-      return false
+    if arg.is_a?(Regexp)
+      my_each { |i| return true unless (i =~ arg).nil? }
+      false
+    elsif arg
+      my_each { |i| return true if i.is_a? arg }
+      false
     end
   end
-
 end
